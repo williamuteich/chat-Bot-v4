17f884d209f798290dc46d47b47eca03e537b4bb
William  Uteich <williamuteich14@gmail.com>
2024-05-17 18:10:26 -0300

    fix: após ultrapassar 2000 caracteres, ele manda o texto por partes.

--- discloud/import/1715863042423/index.js
+++ discloud/import/1715863042423/index.js
@@ -27,6 +27,8 @@ discordClient.on('messageCreate', async (message) => {
         const pegaPergunta = message.content;
         let prompt = pegaPergunta.substring(pegaPergunta.indexOf(" ") + 1);
 
+        prompt += " (. Me mande somente no máximo 2000 caracteres e em português, não pode ultrapassar disso a tua resposta.)";
+
         try {
             const processingMessage = await message.channel.send("Gerando Resposta...");
 
@@ -35,17 +37,13 @@ discordClient.on('messageCreate', async (message) => {
             const response = await result.response;
             const text = await response.text();
 
-            const chunks = text.match(/(.|[\r\n]){1,1999}/g);
-
-            if (chunks && chunks.length > 0) {
-                await processingMessage.edit(`Resposta:\n${chunks[0]}`);
+            // Verifica se o texto é maior que 2000 caracteres
+            if (text.length > 2000) {
+                processingMessage.edit("A resposta gerada é muito longa para ser exibida aqui.");
+            } else {
+                // Envia a resposta do Gemini de volta ao canal do Discord
+                processingMessage.edit(text);
             }
-
-            // Envia cada parte restante como uma mensagem separada
-            for (let i = 1; i < chunks.length; i++) {
-                await message.channel.send(chunks[i]);
-            }
-           
         } catch (error) {
             console.error("Erro ao executar o comando Gemini:", error);
         }
